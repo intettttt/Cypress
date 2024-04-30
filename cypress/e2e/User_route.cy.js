e24607b94ce2895f28de6212c2f46adc8ca7ba35
intet <142960774+intettttt@users.noreply.github.com>
2024-04-30 22:47:39 +0800

    Update User_route.cy.js

--- cypress/e2e/User_route.cy.js
+++ cypress/e2e/User_route.cy.js
@@ -15,71 +15,93 @@ describe('User Test Routes', () => {
 })
 
   describe('User_ID', () => {
-  it('Successfully retrieve user information by ID', () => {
-    cy.visit('https://mariancure-backend.vercel.app/docs#/Users/get_user_api_users__user_id__get')
-    cy.get('.btn').click();
-    cy.get('input').eq(0).type("3");
-    cy.get('.execute-wrapper > .btn').click();
-    
-    cy.request({
-      method: 'GET',
-      url: 'https://mariancure-backend.vercel.app/api/users/3',
-    }).then((response) => {
-      // Assert that the request was successful
-      expect(response.status).to.equal(200);
+    it('Successfully retrieve user information by ID', () => {
+      cy.visit('https://mariancure-backend.vercel.app/docs#/Users/get_user_api_users__user_id__get')
+      cy.get('.btn').click();
+      cy.get('input').eq(0).type("3");
+      cy.get('.execute-wrapper > .btn').click();
+      
+      cy.request({
+        method: 'GET',
+        url: 'https://mariancure-backend.vercel.app/api/users/3',
+      }).then((response) => {
+        // Assert that the request was successful
+        expect(response.status).to.equal(200);
 
-      expect(response.body).to.have.property('username', "VAlvarez");
-      expect(response.body).to.have.property('user_id', 3);
-      expect(response.body).to.have.property('role_id', 1);
-      expect(response.body).to.have.property('role_name', "Patient");
-      expect(response.body).to.have.property('pharmacist_id', null);
-      expect(response.body).to.have.property('pharmacist_name', null);
-    });
-    cy.wait(5000);
+        expect(response.body).to.have.property('username', "VAlvarez");
+        expect(response.body).to.have.property('user_id', 3);
+        expect(response.body).to.have.property('role_id', 1);
+        expect(response.body).to.have.property('role_name', "Patient");
+        expect(response.body).to.have.property('pharmacist_id', null);
+        expect(response.body).to.have.property('pharmacist_name', null);
+      });
+      cy.wait(5000);
+    })
   })
-})
 })
 
   describe('Add_User', () => {
     it('1. Successfully add a new user account.', () => {
-    const string = `{
-      "username": "Princess",
-      "full_name": "Intet",
-      "role_name": "Patient",
-      "password": "Intet123"
-    }`;
+      const string = `{
+        "username": "Princess",
+        "full_name": "Intet",
+        "role_name": "Patient",
+        "password": "Intet123"
+      }`;
   
-        cy.visit('https://mariancure-backend.vercel.app/docs#/Users/add_user_api_add_user_post')
-        cy.get('.try-out > .btn').click();
-        cy.get('.body-param__text').each(($el) => {
-        cy.wrap($el).clear(); // Clear each text box
-        });
-        cy.get('.body-param__text').eq(0).type(string);
-        cy.get('.execute-wrapper > .btn').click();
-        cy.intercept('POST', '/add_user').as('apiRequest');
+      cy.visit('https://mariancure-backend.vercel.app/docs#/Users/add_user_api_add_user_post')
+      cy.get('.try-out > .btn').click();
+      cy.get('.body-param__text').each(($el) => {
+      cy.wrap($el).clear(); // Clear each text box
+      });
+      cy.get('.body-param__text').eq(0).type(string);
+      cy.get('.execute-wrapper > .btn').click();
+      cy.intercept('POST', '/add_user').as('apiRequest');
 
-        cy.wait(5000);
-        });;
+      cy.wait(5000);
+      });;
 
     it('2. Error handling for duplicate user details.', () => {
-    const string = `{
-      "username": "Princess",
-      "full_name": "Intet",
-      "role_name": "Patient",
-      "password": "Intet123"
-    }`;
+      const string = `{
+        "username": "Princess",
+        "full_name": "Intet",
+        "role_name": "Patient",
+        "password": "Intet123"
+      }`;
       
-        cy.visit('https://mariancure-backend.vercel.app/docs#/Users/add_user_api_add_user_post')
-        cy.get('.try-out > .btn').click();
-        cy.get('.body-param__text').each(($el) => {
-        cy.wrap($el).clear(); // Clear each text box
-        });
-    
-        cy.get('.body-param__text').eq(0).type(string);
-        cy.get('.execute-wrapper > .btn').click();
-        cy.intercept('POST', '/add_user').as('apiRequest');
+      cy.visit('https://mariancure-backend.vercel.app/docs#/Users/add_user_api_add_user_post')
+      cy.get('.try-out > .btn').click();
+      cy.get('.body-param__text').each(($el) => {
+      cy.wrap($el).clear(); // Clear each text box
+      });
+      
+      cy.get('.body-param__text').eq(0).type(string);
+      cy.get('.execute-wrapper > .btn').click();
+      cy.intercept('POST', '/add_user').as('apiRequest');
 
-        cy.wait(5000);
-        });
+      cy.wait(5000);
+    });
+
+
+  describe('User_Login', () => {
+    it('1. Successfully retrieve login information.', () => {
+      cy.visit('https://mariancure-backend.vercel.app/docs#/Users/get_user_api_login_get')
+      cy.get('.try-out > .btn').click();
+      cy.get('[data-param-name="username"] > .parameters-col_description > input').eq(0).type("VAlvarez");
+      cy.get('[data-param-name="password"] > .parameters-col_description > input').eq(0).type("Patient1234");
+      cy.get('.execute-wrapper > .btn').click();
+      
+      cy.wait(5000);
+      })
+
+    it('2. Login failed due to incorrect credentials.', () => {
+      cy.visit('https://mariancure-backend.vercel.app/docs#/Users/get_user_api_login_get')
+      cy.get('.try-out > .btn').click();
+      cy.get('[data-param-name="username"] > .parameters-col_description > input').eq(0).type("VAlvarez");
+      cy.get('[data-param-name="password"] > .parameters-col_description > input').eq(0).type("Patient123");
+      cy.get('.execute-wrapper > .btn').click();    
+        
+      cy.wait(5000);
+    })
+  })     
 });
-
