caf896bf9c3f6c84cf290265ecedbf40e10ba479
intet <142960774+intettttt@users.noreply.github.com>
2024-04-30 22:29:33 +0800

    Update User_route.cy.js

--- cypress/e2e/User_route.cy.js
+++ cypress/e2e/User_route.cy.js
@@ -2,9 +2,14 @@ describe('User Test Routes', () => {
   describe('User', () => {
     it('Successfully retrieve user information', () => {
       cy.visit('https://mariancure-backend.vercel.app/docs#/Users/get_users_api_users__get')
+      cy.get('.btn').click();
+      cy.get('.execute-wrapper > .btn').click();
+
       cy.request('https://mariancure-backend.vercel.app/api/users/').then((response)=>{
       expect(response.status).to.eq(200)
       expect(response.body).to.have.length.above(0)
+
+      cy.wait(8000);
       })
   })
 })
@@ -12,7 +17,10 @@ describe('User Test Routes', () => {
   describe('User_ID', () => {
   it('Successfully retrieve user information by ID', () => {
     cy.visit('https://mariancure-backend.vercel.app/docs#/Users/get_user_api_users__user_id__get')
-
+    cy.get('.btn').click();
+    cy.get('input').eq(0).type("3");
+    cy.get('.execute-wrapper > .btn').click();
+    
     cy.request({
       method: 'GET',
       url: 'https://mariancure-backend.vercel.app/api/users/3',
@@ -27,7 +35,9 @@ describe('User Test Routes', () => {
       expect(response.body).to.have.property('pharmacist_id', null);
       expect(response.body).to.have.property('pharmacist_name', null);
     });
+    cy.wait(5000);
   })
+})
 })
 
   describe('Add_User', () => {
@@ -47,6 +57,8 @@ describe('User Test Routes', () => {
         cy.get('.body-param__text').eq(0).type(string);
         cy.get('.execute-wrapper > .btn').click();
         cy.intercept('POST', '/add_user').as('apiRequest');
+
+        cy.wait(5000);
         });;
 
     it('2. Error handling for duplicate user details.', () => {
@@ -64,9 +76,10 @@ describe('User Test Routes', () => {
         });
     
         cy.get('.body-param__text').eq(0).type(string);
-       
         cy.get('.execute-wrapper > .btn').click();
         cy.intercept('POST', '/add_user').as('apiRequest');
+
+        cy.wait(5000);
         });
 });
-})
+
